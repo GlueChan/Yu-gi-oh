c9644a466177cf86264ff2feb2d47e74e4f366d0
ofset <yoshiki.ito.sys@gmail.com>
2017-11-09 11:38:31 +0900

    CoinとDiceの乱数追加

--- app/src/main/java/to/msn/wings/sample/Coin3_screen.java
+++ app/src/main/java/to/msn/wings/sample/Coin3_screen.java
@@ -1,15 +1,21 @@
 package to.msn.wings.sample;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by 4163214 on 10/26/2017.
  */
 
 public class Coin3_screen extends AppCompatActivity implements View.OnClickListener{
+    Button button1;
+    ImageView coin1, coin2, coin3;
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -18,6 +24,13 @@ public class Coin3_screen extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.return_Top).setOnClickListener(this);
         findViewById(R.id.coin_menu).setOnClickListener(this);
+
+        button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        coin1 = (ImageView)findViewById(R.id.coin1);
+        coin2 = (ImageView)findViewById(R.id.coin2);
+        coin3 = (ImageView)findViewById(R.id.coin3);
     }
     public void onClick(View view) {     //ボタンがクリックされたとき
         switch (view.getId()) {
@@ -29,6 +42,24 @@ public class Coin3_screen extends AppCompatActivity implements View.OnClickListe
                 Intent icoinmenu = new Intent(Coin3_screen.this, Coin_menu.class);
                 startActivity(icoinmenu);
                 break;
+        }
+        if (view == button1) {
+            TypedArray typedArray = getResources().obtainTypedArray(R.array.coin);
+            int i = (int)(Math.floor(Math.random() * 2));
+            Drawable drawable = typedArray.getDrawable(i);
+            coin1.setImageDrawable(drawable);
+        }
+        if (view == button1) {
+            TypedArray typedArray = getResources().obtainTypedArray(R.array.coin);
+            int i = (int)(Math.floor(Math.random() * 2));
+            Drawable drawable = typedArray.getDrawable(i);
+            coin2.setImageDrawable(drawable);
+        }
+        if (view == button1) {
+            TypedArray typedArray = getResources().obtainTypedArray(R.array.coin);
+            int i = (int)(Math.floor(Math.random() * 2));
+            Drawable drawable = typedArray.getDrawable(i);
+            coin3.setImageDrawable(drawable);
         }
     }
 }
