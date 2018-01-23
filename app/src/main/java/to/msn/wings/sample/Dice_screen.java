b344925722d99e2e49397e2b35320ef85f733bbf
ofset <yoshiki.ito.sys@gmail.com>
2018-01-23 10:00:42 +0900

    ダイスの遅延とSEの追加

--- app/src/main/java/to/msn/wings/sample/Dice_screen.java
+++ app/src/main/java/to/msn/wings/sample/Dice_screen.java
@@ -1,21 +1,53 @@
 package to.msn.wings.sample;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.os.Handler;
 
 /**
  * Created by 4163214 on 10/26/2017.
  */
 
-public class Dice_screen extends AppCompatActivity implements View.OnClickListener{
+public class Dice_screen extends AppCompatActivity implements View.OnClickListener {
+    private int sound;
     Button button1;
     ImageView dice;
+
+    @SuppressWarnings("deprecation")   // 警告メッセージの抑制
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private SoundPool buildSoundPool(int poolMax) {
+        SoundPool pool = null;
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            pool = new SoundPool(poolMax, AudioManager.STREAM_MUSIC, 0);
+        } else {
+            AudioAttributes attr = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build();
+
+            pool = new SoundPool.Builder()
+                    .setAudioAttributes(attr)
+                    .setMaxStreams(poolMax)
+                    .build();
+        }
+        return pool;
+    }
+
+    final int SOUND_POOL_MAX = 6;
+    SoundPool pool = buildSoundPool(SOUND_POOL_MAX);
+
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -28,19 +60,31 @@ public class Dice_screen extends AppCompatActivity implements View.OnClickListen
         button1.setOnClickListener(this);
 
         dice = (ImageView) findViewById(R.id.dice);
+
+        sound = pool.load(this, R.raw.saikoro, 1);
+
     }
-    public void onClick(View view) {     //ボタンがクリックされたとき
+
+    public void onClick(final View view) {     //ボタンがクリックされたとき
         switch (view.getId()) {
             case R.id.return_Top:       //トップに戻る
                 Intent itop = new Intent(Dice_screen.this, MainActivity.class);
                 startActivity(itop);
                 break;
             case R.id.button1:
-                TypedArray typedArray = getResources().obtainTypedArray(R.array.dice);
-                int i = (int)(Math.floor(Math.random() * 6));
-                Drawable drawable = typedArray.getDrawable(i);
-                dice.setImageDrawable(drawable);
-                break;
+                pool.play(sound, 1.0f, 1.0f, 0, 0, 1);
         }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (view == button1) {
+                    TypedArray typedArray = getResources().obtainTypedArray(R.array.dice);
+                    int i = (int) (Math.floor(Math.random() * 6));
+                    Drawable drawable = typedArray.getDrawable(i);
+                    dice.setImageDrawable(drawable);
+                }
+            }
+        }, 1000);
     }
 }
+
